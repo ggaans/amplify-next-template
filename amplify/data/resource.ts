b5423b47d0364b16db400f7b52e36601d4091c57
ggaans <ggaans@internode.on.net>
2024-11-29 21:38:02 +1030

    handler

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,5 +1,5 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
-import { sayHello } from "../functions/say-hello/resource"
+import { sayHello } from "../say-hello/resource"
 
 /*== STEP 1 ===============================================================
 The section below creates a Todo database table with a "content" field. Try
--- amplify/functions/say-hello/handler.ts
+++
@@ -1,9 +0,0 @@
-import type { Handler } from 'aws-lambda';
-import type { Schema } from "../../data/resource"
-
-export const handler: Schema["sayHello"]["functionHandler"] = async (event) => {
-  // arguments typed from `.arguments()`
-  const { name } = event.arguments
-  // return typed from `.returns()`
-  return `Hello, ${name}!`
-}
